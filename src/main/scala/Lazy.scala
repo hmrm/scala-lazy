d3bc9f818bb2332bfa5cfdd6b76869ad474a3674
Haney Maxwell <hmrmaxwell@gmail.com>
2013-10-06 13:19:42 -0700

    Adding package object and attempt at correct packaging. Also, switched to implicit object for the conversions.

--- src/main/scala/Lazy.scala
+++ src/main/scala/Lazy.scala
@@ -1,16 +1,19 @@
 //God only knows if this is a good idea
-case class Lazy[A](a: Unit => A){
-  lazy val value = a()
-  def apply(): A = value
+package com.hmaxwell.lazydata
+package object Lazy {
+  case class Lazy[A](a: Unit => A){
+    lazy val value = a()
+    def apply(): A = value
 
-  def map[B](f: A => B): Lazy[B] = new Lazy(a andThen f)
-  def flatMap[B](f: A => Lazy[B]): Lazy[B] = {
-    val extract: Lazy[B] => B = _.apply()
-    new Lazy(a andThen f andThen extract)
+    def map[B](f: A => B): Lazy[B] = new Lazy(a andThen f)
+    def flatMap[B](f: A => Lazy[B]): Lazy[B] = {
+      val extract: Lazy[B] => B = _.apply()
+      new Lazy(a andThen f andThen extract)
+    }
   }
-}
 
-object lazyConversions {
-  implicit def strictToLazy[A](a: A): Lazy[A] = new Lazy(_ => a)
-  implicit def lazyToStrict[A](a: Lazy[A]): A = a()
+  implicit object lazyConversions {
+    implicit def strictToLazy[A](a: A): Lazy[A] = new Lazy(_ => a)
+    implicit def lazyToStrict[A](a: Lazy[A]): A = a()
+  }
 }
